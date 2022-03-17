55f4e3c423bbf90cc83edc3539e58c794abe2d7c
Jennifer DE LEO <deleo.jennifer1@gmail.com>
2022-03-17 17:29:23 +0100

    updated html et css + creation listspage.js

---
+++ listspage.js
@@ -0,0 +1,6 @@
+const map = L.map("map").setView([35.6894, 139.692], 12);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>',
+}).addTo(map);
