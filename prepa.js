5f92e6781eb0a8d3a4bb5822979d42a1653b5337
Anna Boval <anna.bo1106@gmail.com>
2022-03-18 18:42:24 +0100

    started JS for check list

--- prepa.js
+++ prepa.js
@@ -6,3 +6,12 @@
   //});
  // line.appendChild(actionElement);
  // tasksTable.appendChild(line);
+
+document.getElementById(completed).checked = true; 
+document.getElementById(completed).checked = false; 
+
+const message = "Bravo, tu es prêt ! ✌️";
+if (completed = true) {
+document.querySelector("#completed").addEventListener('click',function(event){
+   event.preventDefault();
+})};
