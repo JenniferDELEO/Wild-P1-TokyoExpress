131eed6acce93a452429819cca9c5e50dda6eb88
Anna Boval <anna.bo1106@gmail.com>
2022-03-20 19:57:36 +0100

    convertisseur js

--- prepa.js
+++ prepa.js
@@ -7,11 +7,38 @@
  // line.appendChild(actionElement);
  // tasksTable.appendChild(line);
 
-document.getElementById(completed).checked = true; 
-document.getElementById(completed).checked = false; 
-
 const message = "Bravo, tu es prêt ! ✌️";
+
+document.getElementById("completed").checked = true; 
+document.getElementById("completed") = false; 
+
+
 
 document.querySelector(#completed).addEventListener('click',function(event){
    event.preventDefault();
+return message;
 });
+
+
+Euros < input type='text' Name='Euros' onKeyUp='ConvertirEnYen()' Value='1'>
+
+function ConvertirEnYen() {
+if (isNaN(Form1.Euros.value) == true) {
+alert('Merci de saisir un nombre.');
+Form1.Euros.value = '1';
+}
+else {
+Form1.Yen.value = Form1.Euros.value * 0,0076 ; }
+}
+
+Yen <input type ='text' Name='Yen' onKeyUp='ConvertirEnEuros()'>
+function ConvertirEnEuros()
+{
+if (isNaN(Form1.Yen.value) == true) {
+alert('Merci de saisir un nombre.');
+Form1.Yen.value = Form1.Euros.value * 0,0076;
+}
+else {
+Form1.Euros.value = Form1.Yen.value / 0,0076;
+}
+}
