508773a2395841d3d34471742ed57658a8349d8f
Anna Boval <anna.bo1106@gmail.com>
2022-03-21 16:44:49 +0100

    changed some CSS

--- prepa.js
+++ prepa.js
@@ -7,38 +7,22 @@
  // line.appendChild(actionElement);
  // tasksTable.appendChild(line);
 
-const message = "Bravo, tu es prêt ! ✌️";
-
-document.getElementById("completed").checked = true; 
-document.getElementById("completed") = false; 
-
-
-
-document.querySelector(#completed).addEventListener('click',function(event){
-   event.preventDefault();
-return message;
-});
+//const message = "Bravo, tu es prêt ! ✌️";
+//document.getElementById("completed").checked = true; 
+//document.getElementById("completed") = false; 
+//document.querySelector(#completed).addEventListener('click',function(event){
+  // event.preventDefault();
+//return message;
+//});
 
 
-Euros < input type='text' Name='Euros' onKeyUp='ConvertirEnYen()' Value='1'>
+Euros < input; type ='text'; Name ='Euros'; onKeyUp ='ConvertirEnYen()'; Value ='1'>
 
 function ConvertirEnYen() {
-if (isNaN(Form1.Euros.value) == true) {
+if (isNaN(form-yen.Euros.value) == true) {
 alert('Merci de saisir un nombre.');
-Form1.Euros.value = '1';
+form-yen.Euros.value = '1' ;
 }
 else {
-Form1.Yen.value = Form1.Euros.value * 0,0076 ; }
-}
-
-Yen <input type ='text' Name='Yen' onKeyUp='ConvertirEnEuros()'>
-function ConvertirEnEuros()
-{
-if (isNaN(Form1.Yen.value) == true) {
-alert('Merci de saisir un nombre.');
-Form1.Yen.value = Form1.Euros.value * 0,0076;
-}
-else {
-Form1.Euros.value = Form1.Yen.value / 0,0076;
-}
+form-yen.Yen.value = form-yen.Euros.value * 0,0076 ; }
 }
