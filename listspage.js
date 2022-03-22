94be624c0d5df223a422630a05340326b06dabed
Jennifer DE LEO <deleo.jennifer1@gmail.com>
2022-03-22 15:33:50 +0100

    updated map items

--- listspage.js
+++ listspage.js
@@ -7,3 +7,10 @@ L.tileLayer("https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png", {
   attribution:
     '&copy; OpenStreetMap France | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
+
+L.marker([35.6861, 139.7149])
+  .addTo(map)
+  .bindPopup(
+    "<h4>Les jardins de Shinjuku</h4> 11 Naitomachi, Shinjuku City, Tokyo 160-0014, Japon"
+  )
+  .openPopup();
