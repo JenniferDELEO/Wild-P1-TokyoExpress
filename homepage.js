e6c80c58000ea0f193e63e149b261f05010d3513
leolab69 <leolaborie123@gmail.com>
2022-03-24 09:36:38 +0100

    add dark mode + level up index

--- homepage.js
+++ homepage.js
@@ -1,5 +1,9 @@
-const image = document.getElementsByClassName('imgparallax');
-new simpleParallax(image, {
-  delay: 0.7,
-  transition: 'cubic-bezier(0,0,0,2)',
-});
+const icon = document.getElementById('icon');
+icon.onclick = function () {
+  document.body.classList.toggle('dark-theme');
+  if (document.body.classList.contains('dark-theme')) {
+    icon.src = 'assets/sun.png';
+  } else {
+    icon.src = 'assets/lunes.png';
+  }
+};
